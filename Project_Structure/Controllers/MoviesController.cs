268ec1fdd896e361c25942d7f87155d8c565bf10
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-02-20 12:58:45 +0200

    Session02 Demo

---
+++ Project_Structure/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Structure.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public void GetMovie(int id)
+        {
+
+        }
+        public string Index()
+        {
+            return "Hello From Index";
+        }
+    }
+}
